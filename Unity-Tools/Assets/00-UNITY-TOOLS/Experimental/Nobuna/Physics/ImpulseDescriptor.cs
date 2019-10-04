8f14a7607b2c74928738b82861d4e3ba970f04da
Morgan Hoarau <morgansb.hoarau@gmail.com>
2019-10-04 14:33:40 +0200

    [Exp] local only

---
+++ Unity-Tools/Assets/00-UNITY-TOOLS/Experimental/Nobuna/Physics/ImpulseDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "[ImpulseDesc]", menuName = "Framework Data/Physics/Impulse")]
+public class ImpulseDescriptor : ScriptableObject
+{
+    public Vector3 Force = Vector3.zero;
+    public float Duration = -1f;
+    public UnityAction OnImpulseEnd;
+}
---
+++ Unity-Tools/Assets/00-UNITY-TOOLS/Production/Sources/FiniteStateMachine/README.md
@@ -0,0 +1,8 @@
+# Unity-Tools Finite State Machine
+
+v1.0: 
+- First version.
+
+v1.1:
+- <Update> FSMStateModule: When generating a preset, auto-assign it.
+- <Fix> FSMStateModule_Move2D:  line 108: if(**m_Preset &&** m_Preset.Data != null)
